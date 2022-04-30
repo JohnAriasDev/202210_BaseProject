db2d7004371ab1fd067299955ffef069d0fbf23e
JohnAriasDev <ja.ariasa1@uniandes.edu.co>
2022-04-30 12:35:51 -0500

    version incial adicion de servicios y modulos

---
+++ src/app/vehiculo/vehiculo.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Vehiculo } from './Vehiculo';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VehiculoService {
+  private apiUrl: string = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getVehiculos():Observable<Vehiculo[]>{
+    return this.http.get<Vehiculo[]>(this.apiUrl);
+  }
+
+}
